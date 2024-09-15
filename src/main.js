df3a50c724f2e5ab645a2cd04c3009f8722ecda7
ssa <3188545392@qq.com>
2024-09-15 17:09:47 +0800

    搭建登录页面

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.scss'
+import SvgIcon from './icons'
 
 // createApp创建一个vue应用实例，App是父组件
 // use是使用插件，store是vuex插件，router是路由插件
 // mount挂载到id为app的元素上
-createApp(App).use(store).use(router).mount('#app')
+const app =  createApp(App)
+SvgIcon(app)
+app.use(store).use(router).mount('#app')
